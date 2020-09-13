d67a45e835db4fe43e6e7bfde749633e379af4a5
yhd <tkwkd77741@gmail.com>
2020-09-13 22:11:09 +0900

    재실행

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.jojoldu.book.springboot.config.auth.dto;
 import com.jojoldu.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
@@ -16,3 +18,4 @@ public class SessionUser {
         this.picture = user.getPicture();
     }
 }
+
